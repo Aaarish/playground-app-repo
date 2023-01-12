2133c1b80c9b62756408ba4f5b0b73394dc59757
Aarish <aarishm767@gmail.com>
2023-01-12 16:56:37 +0530

    Initial Commit

---
+++ src/test/java/com/example/PlaygroundApp/PlaygroundAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlaygroundApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
